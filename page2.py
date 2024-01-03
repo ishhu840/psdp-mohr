542c9e99580027bb36abbfd8ca5a9a13b7f7b281
ishhu840 <54225074+ishhu840@users.noreply.github.com>
2024-01-03 11:35:03 +0500

    Update page2.py

--- page2.py
+++ page2.py
@@ -8,6 +8,8 @@ import plotly.graph_objects as go
 import Functions as tp
 from streamlit_option_menu import option_menu
 import Functions as objFun
+import altair as alt
+
 
 larger_font_size = "50px"
 
@@ -25,14 +27,14 @@ def styled_box(icon, title, value):
             </div>
         """
         
-df = pd.read_excel(
+""" df = pd.read_excel(
     io="psdp.xlsx",
     engine='openpyxl',
     sheet_name='Umbrella',
     skiprows=0,
     usecols='A:F',
     nrows=50000,
-)
+) """
 
 def app():
     
@@ -56,7 +58,7 @@ def app():
     )
      
     
-    selected_option = option_menu(None, ["Budget Execution (2023-24)", "Physical & Financial Detail", 'Monitoring Report'], 
+    selected_option = option_menu(None, ["Budget Execution (2023-24)", "Projects < 70% Expenditure", 'Projects Headwise Expenditure'], 
         icons=['house', "list-task", 'gear'], 
         menu_icon="cast", default_index=0, orientation="horizontal",
         styles={
@@ -79,21 +81,22 @@ def app():
 
         total_budget = df_selection ['Original Budget'].sum() / 1000000
         released_budget = df_selection ['Released Budget'].sum() / 1000000
-        total_expenditure = df_selection ['Expenditure'].sum() / 1000000
+        Expenditure = df_selection ['Expenditure'].sum() / 1000000
         
 
         st.markdown("---")
-        objFun.projectInfoCard(total_budget,released_budget,total_expenditure )
+        objFun.projectInfoCard(total_budget,released_budget,Expenditure )
 
         st.markdown("---")
         
 
-        objFun.circulechart(total_budget,released_budget,total_expenditure,selected_project)
+        objFun.circulechart(total_budget,released_budget,Expenditure,selected_project)
         
 
         st.markdown("---")
 
-    ##################
+    ############################################################################
+    
         df_grouped = df_selection.groupby('head')[['Expenditure', 'Released Budget']].sum().reset_index()
 
         # Plot Bars
@@ -121,21 +124,144 @@ def app():
         st.pyplot(plt)
             # You can add your charts, data, or any other content specific to this option here.
 
-    elif selected_option == "Physical & Financial Detail":
-        # Add your code for the 'Physical & Financial Detail' option here
-        st.write("Displaying Physical & Financial Detail data")
-        # You can add your charts, data, or any other content specific to this option here.
+    elif selected_option == "Projects < 70% Expenditure":
+        # Add your code for the 'Physical & Financial Detail' option here   
 
-    elif selected_option == 'Monitoring Report':
-        # Add your code for the 'Monitoring Report' option here
-        st.write("Displaying Monitoring Report data")
-        # You can add your charts, data, or any other content specific to this option here.
+     #   st.title(' List of Fund Centers, < then 70 % Expenditure of Released Budget')
 
-    # You can add an 'else' block for handling unexpected options if needed.
+        # Upload an Excel file
+        # Group data by 'Project' and sum the 'Release Budget' and 'Expenditure' columns
+###############################################################################################
+        projects_below_70_list = []
+        headwise_below_70_list = []
 
+        # Iterate through unique project names
+        for project_name in df['projectname'].unique():
+            project_data = df[df['projectname'] == project_name]
 
-################
+            # Calculate total release and expenditure for the current project
+            Release = project_data['Released Budget'].sum()
+            Expenditure = project_data['Expenditure'].sum()
 
+            # Check if the project has overall expenditure less than 70%
+            if Expenditure < 0.7 * Release:
+                # Calculate the percentage
+                percentage = round((Expenditure / Release) * 100)
+
+                projects_below_70_list.append({
+                    'ProjectName': project_name,
+                    'Release': Release / 1000000,  # Convert to millions
+                    'Expenditure': Expenditure / 1000000,  # Convert to millions
+                    'Percentage': percentage
+                })
+
+        # Create DataFrame from the collected results
+            projects_below_70_df = pd.DataFrame(projects_below_70_list)
+
+            # Modify the DataFrame for display
+            projects_below_70_df_display = projects_below_70_df.copy()
+            projects_below_70_df_display['Release'] = projects_below_70_df['Release'].apply(lambda x: f"{x:.2f} ")
+            projects_below_70_df_display['Expenditure'] = projects_below_70_df['Expenditure'].apply(lambda x: f"{x:.2f} ")
+            projects_below_70_df_display['Percentage'] = projects_below_70_df['Percentage'].astype(str) + '%'
+
+            # Function to apply styling to the 'Percentage' column
+            def format_percentage(val):
+                try:
+                    percentage_value = float(val[:-1])
+                    if percentage_value < 30:
+                        return 'font-weight: bold; color: red'
+                    else:
+                        return 'font-weight: bold; color: black'
+                except ValueError:
+                    return 'font-weight: bold; color: black'
+
+            # Display results in Streamlit
+        st.write("Projects with Overall Expenditure Less Than 70%:")
+        st.table(projects_below_70_df_display.style.applymap(format_percentage, subset=['Percentage']).set_table_styles([{'selector': 'th', 'props': [('font-size', '150%')]}]))
+
+###############################################################################################
+       
+    elif selected_option == 'Projects Headwise Expenditure':
+        
+        projects_below_70_list = []
+        headwise_below_70_list = []
+
+        # Iterate through unique project names
+        for project_name in df['projectname'].unique():
+            project_data = df[df['projectname'] == project_name]
+
+            # Calculate total release and expenditure for the current project
+            Release = project_data['Released Budget'].sum()
+            Expenditure = project_data['Expenditure'].sum()
+
+            # Check if the project has overall expenditure less than 70%
+            if Expenditure < 0.7 * Release:
+                # Calculate the percentage
+                percentage = round((Expenditure / Release) * 100)
+
+                projects_below_70_list.append({
+                    'ProjectName': project_name,
+                    'Release': Release / 1000000,  # Convert to millions
+                    'Expenditure': Expenditure / 1000000,  # Convert to millions
+                    'Percentage': percentage
+                })
+
+                # Identify heads with less than 70% expenditure for the current project
+                heads_below_70 = project_data[project_data['Expenditure'] < 0.7 * project_data['Released Budget']]
+                if not heads_below_70.empty:
+                    headwise_below_70_list.append({
+                        'ProjectName': project_name,
+                        'Release': Release / 1000000,  # Convert to millions
+                        'Heads_Below_70': heads_below_70[['head', 'Released Budget', 'Expenditure']].copy()
+                    })
+
+        # Create DataFrames from the collected results
+        projects_below_70_df = pd.DataFrame(projects_below_70_list)
+
+        # Modify the DataFrame for display
+        projects_below_70_df_display = projects_below_70_df.copy()
+        projects_below_70_df_display['Release'] = projects_below_70_df['Release'].apply(lambda x: f"{x:.2f} ")
+        projects_below_70_df_display['Expenditure'] = projects_below_70_df['Expenditure'].apply(lambda x: f"{x:.2f} ")
+        projects_below_70_df_display['Percentage'] = projects_below_70_df['Percentage'].astype(str) + '%'
+
+        # Display results in Streamlit
+        st.write("Headwise Expenditure Below 70% for Each Project:")
+
+        # Display a list of projects with expandable sections for details
+        for index, row in enumerate(headwise_below_70_list):
+            expander = st.expander(f"Project: {row['ProjectName']}")
+            expander.write(f"Total Release: {row['Release']:.2f} million")  # Display in millions with 2 decimal places
+
+            # Check if 'Released Budget' and 'Expenditure' are the correct column names in your DataFrame
+            if 'Released Budget' in row['Heads_Below_70'].columns and 'Expenditure' in row['Heads_Below_70'].columns:
+                # Display a table for heads below 70% expenditure using Plotly
+                heads_table = row['Heads_Below_70']
+
+                # Check if 'Expenditure Percentage' column exists before conversion
+                if 'Expenditure Percentage' in heads_table.columns:
+                    heads_table['Expenditure Percentage'] = pd.to_numeric(heads_table['Expenditure Percentage'], errors='coerce')
+
+                # Calculate the 'Expenditure Percentage' for each head
+                heads_table['Expenditure Percentage'] = (heads_table['Expenditure'] / heads_table['Released Budget']) * 100
+
+                # Apply background color based on the condition
+                heads_table_style = heads_table.style.applymap(
+                    lambda x: 'background-color: #FFC8C8' if pd.notna(x) and x < 50 else '',
+                    subset=['Expenditure Percentage']
+                ).format({'Expenditure Percentage': '{:.2f}%'}).set_table_styles([
+                    {'selector': 'th', 'props': [('font-size', '18px'), ('background-color', '#3498db'), ('color', 'white')]},
+                    {'selector': 'td', 'props': [('font-size', '16px')]}
+                ])
+
+                # Display the styled table
+                expander.table(heads_table_style)
+                #expander.dataframe(heads_table_style)
+
+            else:
+                expander.warning("Column names 'Released Budget' and 'Expenditure' not found in the data.")
+        
+
+###########################
 if __name__ == '__main__':
     app()
 
