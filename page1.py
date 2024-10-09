dd107d9e110d8a8a868c129c068729f5481624a0
ishhu840 <54225074+ishhu840@users.noreply.github.com>
2024-10-10 04:35:38 +0500

    Update page1.py

--- page1.py
+++ page1.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import plotly.express as px
 import random
 import warnings
 warnings.filterwarnings("ignore")
@@ -20,22 +19,22 @@ def app():
                 """
     st.markdown(hide_st_style, unsafe_allow_html=True)
 
-    df =  pd.read_excel(
+    df = pd.read_excel(
         io="psdp.xlsx",
         engine='openpyxl',
         sheet_name='data',
-        skiprows= 0,
+        skiprows=0,
         usecols='A:L',
         nrows=50000,
     )
 
-    st.title(":bar_chart: PSDP (12) on Going Projects ( Budget Execution) FY 2023-24")
+    st.title(":bar_chart: PSDP (12) on Going Projects (Budget Execution) FY 2023-24")
     st.markdown("#")
 
     total_budget = df['Original Budget'].sum() / 1000000
     released_budget = df['Released Budget'].sum() / 1000000
     total_expenditure = df['Expenditure'].sum() / 1000000
-    
+
     st.markdown("---")
     Functions.printinfo(total_budget, released_budget, total_expenditure)
     st.markdown("---")
@@ -45,7 +44,7 @@ def app():
 
     with left_column:
         labels = 'Total Budget', 'Released Budget'
-        sizes = [total_budget - released_budget, released_budget]
+        sizes = [max(total_budget - released_budget, 0), released_budget]  # Ensure non-negative values
         explode = (0, 0.1)
         fig1, ax1 = plt.subplots()
         ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.01f%%',
@@ -56,9 +55,9 @@ def app():
 
     with right_column:
         labels = 'Released Budget', 'Expenditure'
-        expenditure_percentage = (total_expenditure / released_budget) * 100
+        expenditure_percentage = (total_expenditure / released_budget) * 100 if released_budget > 0 else 0
 
-        # Check if expenditure exceeds released budget
+        # Handle cases where expenditure exceeds released budget
         if total_expenditure > released_budget:
             expenditure_percentage = 100  # Expenditure exceeds or equals release
             remaining_percentage = 0      # No remaining budget
@@ -67,12 +66,12 @@ def app():
 
         sizes = [remaining_percentage, expenditure_percentage]
         explode = (0, 0.1)
-        fig1, ax1 = plt.subplots()
-        ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.01f%%',
+        fig2, ax2 = plt.subplots()
+        ax2.pie(sizes, explode=explode, labels=labels, autopct='%1.01f%%',
                 shadow=False, startangle=60)
-        ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+        ax2.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
         plt.title("Expenditure vs. Budget Released %")
-        st.pyplot(fig1)
+        st.pyplot(fig2)
 
     st.markdown("##")
     st.markdown("---")
